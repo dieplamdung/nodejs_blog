cdb3f9376d7babc607e8d53c9dbe13b5bd37c9e3
dieplamdung <34808912+dieplamdung@users.noreply.github.com>
2021-03-31 17:27:06 +0700

    test husky

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const newController = require('../app/controllers/NewsController');
 
-            router.use('/:slug', newController.show);
+router.use('/:slug', newController.show);
 
-            router.use('/', newController.index);
+router.use('/', newController.index);
 
 module.exports = router;
